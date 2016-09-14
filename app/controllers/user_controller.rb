16b4ed6dd09ad638c4c8ac6d6b0c9ab53287724c
sami4thewin <samiomarmohammed@gmail.com>
2016-09-14 15:51:06 -0400

    Working on authenticating

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,10 +6,16 @@ class UserController < ApplicationController
     erb :'/users/signup'
   end
 
-  post '/users' do
+  post '/users/signup' do
     submitted_email = params[:email]
-    binding.pry
+    # binding.pry
     if submitted_email.match(VALID_EMAIL_REGEX) != nil
+      User.all.each do |user|
+        if user.email == submitted_email
+          redirect to '/users/login'
+          #flash message saying this email has an account
+        end
+      end
       @user = User.find_or_create_by(params)
       session[:id] = @user.id
       redirect '/users/home'
@@ -19,8 +25,31 @@ class UserController < ApplicationController
     end
   end
 
+
+  # User.all.each do |user|
+  #   if user.email == submitted_email
+  #     redirect to '/users/login'
+  #     #flash message saying this email has an account
+  #   end
+  # end
+
+  # ary = []
+  # code bla bla bla
+  # user_email == email already registerd
+  # ary << email
+  # bla bla bla end
+  #
+  # ary.empty?
+
+
   get '/users/login' do
     erb :'/users/login'
+  end
+
+  post '/users/login' do
+    @user = User.find_by(params)
+    session[:id] = @user.id
+    redirect '/users/home'
   end
 
   get '/users/home' do
